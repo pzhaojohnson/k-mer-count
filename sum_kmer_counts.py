316e9afa61fe436b83cd8875c7665a586c028a53
Philip Johnson <p.zhao.johnson@gmail.com>
2023-05-25 09:39:19 -0400

    Use deep_copy_kmer_counts function in sum_kmer_counts function

--- sum_kmer_counts.py
+++ sum_kmer_counts.py
@@ -1,3 +1,5 @@
+from deep_copy_kmer_counts import deep_copy_kmer_counts
+
 def sum_kmer_counts(counts1, counts2):
     """Returns a dictionary of kmer count sums.
 
@@ -12,10 +14,7 @@ def sum_kmer_counts(counts1, counts2):
     counts1 -- the first kmer counts dictionary
     counts2 -- the second kmer counts dictionary
     """
-    count_sums = {}
-    # add all kmer counts in the first dictionary
-    for kmer1, ct1 in counts1.items():
-        count_sums[kmer1] = ct1
+    count_sums = deep_copy_kmer_counts(counts1)
     # add all kmer counts in the second dictionary
     for kmer2, ct2 in counts2.items():
         ct_sum = count_sums[kmer2] if kmer2 in count_sums else 0
